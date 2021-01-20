e14314969f9b3279d64bb7079d31388a9564da8e
olehan <alihan.arshabekov@gmail.com>
2021-01-20 13:55:00 +0600

    feat(eslint-config-typescript): add proper tests

--- tests/eslint-config-typescript.test.ts
+++ tests/eslint-config-typescript.test.ts
@@ -1,44 +1,85 @@
-import * as config from '@azimutlabs/eslint-config-typescript';
+import * as configBase from '@azimutlabs/eslint-config-typescript/base';
+import * as configDeclarations from '@azimutlabs/eslint-config-typescript/declarations';
+import * as http from 'http';
+
+http.createServer()
 
 import { buildESLint } from './services/builders';
 import { getMessagesFromLintResults } from './services/getMessagesFromLintResults';
+import type { Linter } from 'eslint';
 
-const eslint = buildESLint(config);
+const withCreateDefaultProgram = (config: Linter.Config): Linter.Config => ({
+  ...config,
+  parserOptions: {
+    ...config.parserOptions,
+    /**
+     * Couldn't make @typescript-plugin/parser to believe the linted code is included by tsconfig,
+     * so had no option to make test suits to run with `createDefaultProgram`.
+     * @see https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#parseroptionscreatedefaultprogram
+     */
+    createDefaultProgram: true,
+  },
+});
 
-// TODO: these cases do not actually test
+const eslintBase = buildESLint(withCreateDefaultProgram(configBase));
+const eslintDeclarations = buildESLint(withCreateDefaultProgram(configBase), {
+  overrideConfig: configDeclarations,
+});
+
 describe('successful cases', () => {
   it('should lint a node.js http server start', async () => {
-    const file = `const http = require('http');
+    const file = `import * as http from 'http';
 
 const statusOk = 200;
 const port = 8080;
 
-const requestListener = (req, res) => {
+const requestListener: http.RequestListener = (_, res) => {
   res.writeHead(statusOk);
-  res.end('Hello, World!');
+  res.end(http.STATUS_CODES[statusOk]);
 };
 
 http.createServer(requestListener).listen(port);
 `;
-    expect(getMessagesFromLintResults(await eslint.lintText(file))).toEqual([]);
+    expect(getMessagesFromLintResults(await eslintBase.lintText(file))).toEqual([]);
   });
 
   it('should lint a client-side react component without jsx', async () => {
-    const file = `import styled from '@emotion/styled';
-import { createElement } from 'react';
+    const file = `import { createElement } from 'react';
+import type { FC } from 'react';
 
-const ButtonStyled = styled.button({
-  backgroundColor: 'red',
-  borderRadius: 4,
-});
+enum Colors {
+  Blue = 'blue',
+  Pink = 'pink',
+  Red = 'red',
+}
 
-export const Button = ({ children, color: _color, ...rest }) =>
-  createElement(ButtonStyled, rest, children);
+type AnyColor = string;
+type Color = AnyColor | Colors;
+
+interface ColorProps<C extends Color = AnyColor> {
+  readonly color: C;
+}
+
+interface ButtonProps extends ColorProps<Colors.Blue | Colors.Red> {}
+
+export const Button: FC<ButtonProps> = ({ children, color: _color, ...rest }) =>
+  createElement(
+    'button',
+    { ...rest, 'aria-disabled': disabled ? 'disabled' : 'not' },
+    children,
+  );
 
 Button.defaultProps = {
   disabled: true,
 };
 `;
-    expect(getMessagesFromLintResults(await eslint.lintText(file))).toEqual([]);
+    expect(getMessagesFromLintResults(await eslintBase.lintText(file))).toEqual([]);
+  });
+
+  it('should lint a next-env.d.ts', async () => {
+    const file = `/// <reference types="next" />
+/// <reference types="next/types/global" />
+`;
+    expect(getMessagesFromLintResults(await eslintDeclarations.lintText(file))).toEqual([]);
   });
 });
